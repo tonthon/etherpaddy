e0a217babfc1bbf22ed2e5584c9eec458e57e80c
tonthon <tonthon21@gmail.com>
2012-03-16 00:22:47 +0100

    Adding bootstrap and other js libs

---
+++ etherpadpadlist/static/js/etherpad.js
@@ -0,0 +1,53 @@
+(function( $ ){
+
+  $.fn.pad = function( options ) {
+    var settings = {
+      'host'              : 'http://etherpad.majerti.fr',
+      'baseUrl'           : '/p/',
+      'showControls'      : false,
+      'showChat'          : false,
+      'showLineNumbers'   : false,
+      'userName'          : 'unnamed',
+      'useMonospaceFont'  : false,
+      'noColors'          : false,
+      'hideQRCode'        : false,
+      'width'             : "100%",
+      'height'            : "100%",
+      'border'            : 0,
+      'borderStyle'       : 'solid'
+    };
+
+    // This writes a new frame if required
+
+    var frameID = this.attr('id');
+    var epframe = "epframe"+frameID;
+    if ( !options.getContents )
+    {
+      if ( options )
+      {
+        $.extend( settings, options );
+      }
+      var iFrameLink = '<iframe id="' + epframe + '" src="'+settings.host+settings.baseUrl+settings.padId+'?showControls='+settings.showControls+'&showChat='+settings.showChat+'&showLineNumbers='+settings.showLineNumbers+'&useMonospaceFont='+settings.useMonospaceFont+'&userName=' + settings.userName + '&noColors=' + settings.noColors + '&hideQRCode=' + settings.hideQRCode + '" style="border: '+settings.border+'; border-style: '+settings.borderStyle+'; width: '+settings.width+'; height: '+settings.height+';"></iframe>';
+      // console.log(iFrameLink);
+      this.html(iFrameLink);
+    }
+    // This reads the etherpad contents if required
+    else if ( options.getContents )
+    {
+      // Specify the target Div
+      var targetDiv = options.getContents;
+
+      // Get the frame properties and provide us with an export path
+      var frameUrl = document.getElementById(epframe).src;
+      if (frameUrl.indexOf("?")>-1){
+        frameUrl = frameUrl.substr(0,frameUrl.indexOf("?"));
+      }
+      var contentsUrl = frameUrl + "/export/html";
+
+      // perform an ajax call on contentsUrl and write it to the parent
+      $.get(contentsUrl, function(data) {
+      $('#'+targetDiv).html(data);
+      });
+    }
+  };
+})( jQuery );
