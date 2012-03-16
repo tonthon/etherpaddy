5110261bb9844d92aefc4f02aa3a3c291ef268cf
tonthon <tonthon21@gmail.com>
2012-03-16 11:49:43 +0100

    Moving conf and adding customization: * Apache sample configuration moved to apache folder * Etherpad-lite customization for fullscreen mode

---
+++ etherpad-lite/static/custom/pad.js
@@ -0,0 +1,38 @@
+/*
+ * File Name : pad.js
+ *
+ * Copyright (C) 2012 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
+function goToPadList(){
+  /*
+   * Launched on menu item click, redirect the user to the padlist
+   */
+  if (window.confirm("Go to padlist and leave the document ?")){
+    window.location = '/pads';
+  }
+  return false;
+}
+function getCustomMenuItems(){
+  /*
+   * Return the custom menu Item
+   */
+  return "<li onclick=\"goToPadList();return false;\"> \
+    <a class='buttonicon buttonicon-insertunorderedlist'></a> \
+    </li>";
+
+}
+
+function customStart()
+{
+  /*
+   * Etherpad-lite built-in function to hack pad's startup
+   * add a custom button
+   */
+  var right_menu = $('#menu_right');
+  right_menu.prepend(getCustomMenuItems);
+}
