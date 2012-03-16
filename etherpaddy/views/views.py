3af46521909ec44247ba2342dcba368cf17f3728
tonthon <tonthon21@gmail.com>
2012-03-16 18:27:49 +0100

    Random name and delete view

--- etherpaddy/views/views.py
+++ etherpaddy/views/views.py
@@ -6,18 +6,34 @@
 #   License: http://www.gnu.org/licenses/gpl-3.0.txt
 #
 # * Creation Date : mer. 11 janv. 2012
-# * Last Modified : ven. 16 mars 2012 00:34:14 CET
+# * Last Modified : ven. 16 mars 2012 18:12:26 CET
 #
 # * Project : etherpaddy
 #
 """
     Main views for etherpad-lite
 """
+import string
+import random
+from urllib2 import URLError
 
+from pyramid.threadlocal import get_current_registry
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.url import route_path
+
+from etherpaddy.external.py_etherpad import EtherpadLiteClient
 
 from etherpaddy.models.model import get_all_pads
 from etherpaddy.jsonify import jsonify
+
+def gen_random():
+    """
+        Returns a random string composed with digits and characters
+    """
+    rand = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                                                        for x in range(10))
+    return "random_" + rand
 
 @view_config(route_name='index', renderer='index.mako')
 def default_index(request):
@@ -53,3 +69,33 @@ def newpad(request):
         Returns the pad add form
     """
     return dict(title='Add a new pad')
+
+@view_config(route_name='pad', renderer='json',
+                                    request_method='DELETE')
+def deletepad(request):
+    """
+        Delete a pad
+    """
+    padid = request.matchdict.get('padid')
+    settings = get_current_registry().settings
+    host = settings['etherpaddy.host']
+    apikey = settings['etherpaddy.apikey']
+    try:
+        api = EtherpadLiteClient(apikey, host)
+        ret_datas = api.deletePad(padid)
+    except URLError:
+        ret_datas = dict(code=1,
+                    message="Unable to join etherpad-lite's api, \
+check configuration and network connection")
+    return ret_datas
+
+@view_config(route_name='genpadname', renderer='json', request_method='GET')
+def genpadname(request):
+    """
+        Redirect to a new pad with a random name
+    """
+    all_pads = [p.get_name() for p in get_all_pads()]
+    name = gen_random()
+    while name in all_pads:
+        name = gen_random()
+    return HTTPFound(route_path('pad', request, padid=name))
