3f0a694eb9ef25e17ac714e8282c45e5dcd6c5f4
tonthon <tonthon21@gmail.com>
2012-03-21 11:12:20 +0100

    Fix padlist regexp to match all possible names

--- etherpaddy/models/model.py
+++ etherpaddy/models/model.py
@@ -6,7 +6,11 @@
 #   License: http://www.gnu.org/licenses/gpl-3.0.txt
 #
 # * Creation Date : mer. 11 janv. 2012
+<<<<<<< HEAD
 # * Last Modified : lun. 19 mars 2012 10:57:46 CET
+=======
+# * Last Modified : mer. 21 mars 2012 10:58:28 CET
+>>>>>>> Fix padlist regexp to match all possible names
 #
 # * Project : etherpaddy
 #
@@ -29,5 +33,11 @@ def get_all_pads():
     """
         Return all existing pads from the database
     """
+<<<<<<< HEAD
     padregex = "^pad:{1}[a-zA-Z0-9_-]+$"
     return DBSESSION().query(StoreItem).filter(StoreItem.key.op('regexp')(padregex))
+=======
+    padregex = "^(pad:){1}[^:]+$"
+    return DBSESSION().query(StoreItem).filter(
+            StoreItem.key.op('regexp')(padregex))
+>>>>>>> Fix padlist regexp to match all possible names
