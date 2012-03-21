332d7d8dcaec239f50d67a29d5f435311fe7ac8a
tonthon <tonthon21@gmail.com>
2012-03-21 12:23:59 +0100

    Merging conflict

--- etherpaddy/models/model.py
+++ etherpaddy/models/model.py
@@ -6,11 +6,7 @@
 #   License: http://www.gnu.org/licenses/gpl-3.0.txt
 #
 # * Creation Date : mer. 11 janv. 2012
-<<<<<<< HEAD
-# * Last Modified : lun. 19 mars 2012 10:57:46 CET
-=======
-# * Last Modified : mer. 21 mars 2012 10:58:28 CET
->>>>>>> Fix padlist regexp to match all possible names
+# * Last Modified : mer. 21 mars 2012 12:23:40 CET
 #
 # * Project : etherpaddy
 #
@@ -33,11 +29,6 @@ def get_all_pads():
     """
         Return all existing pads from the database
     """
-<<<<<<< HEAD
-    padregex = "^pad:{1}[a-zA-Z0-9_-]+$"
-    return DBSESSION().query(StoreItem).filter(StoreItem.key.op('regexp')(padregex))
-=======
     padregex = "^(pad:){1}[^:]+$"
     return DBSESSION().query(StoreItem).filter(
             StoreItem.key.op('regexp')(padregex))
->>>>>>> Fix padlist regexp to match all possible names
